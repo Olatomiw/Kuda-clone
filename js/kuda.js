c8bdf592eb4a6a5649c6272e04762c3a97839307
DevTom <towolawiridwan@gmail.com>
2022-07-05 10:13:33 +0100

    first commit

---
+++ js/kuda.js
@@ -0,0 +1,16 @@
+var y = document.getElementById("icon")
+
+    // window.addEventListener("load", myResize);
+    // window.addEventListener("resize", myResize);
+    // function myResize(){
+    //     var y = document.getElementById("feat");
+    //     if (innerWidth <=990) {
+    //         y.innerHTML = "Features";
+            
+    //     }
+    //     else if (innerWidth >=991) {
+    //         y.innerHTML = "Personal"
+    //     }
+
+
+    // }
